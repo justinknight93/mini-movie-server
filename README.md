50139ac3bf338dc97a6f8551cd78f031ffb5fb57
justinknight_bespin <justin.knight.14@us.af.mil>
2023-01-05 10:13:15 -0600

    updated directory page

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 A simple node program that creates a http server for Kodi.
 Not a very impressive program but it works. :)
 
-With Node.js installed run
+Add whatever you want to the movies folder and (with Node.js installed) run the following commands
 
 ```
 npm install
 npm start
 ```
 
-Port number and folder path can be changed by editing the .env file.
+Port number and folder name can be changed by editing the .env file.
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import * as dotenv from "dotenv";
-import serveIndex from "serve-index";
+import * as path from "path";
+import * as fs from "fs";
 import express from "express";
 import ip from "ip";
+
+let files = [];
+
+function scanDirectory(directory) {
+  fs.readdirSync(directory).forEach((file) => {
+    const fullPath = path.join(directory, file);
+    if (fs.statSync(fullPath).isDirectory()) return scanDirectory(fullPath);
+    else return files.push(fullPath);
+  });
+}
 
 dotenv.config();
 
@@ -10,7 +21,14 @@ const folder = env.FOLDER_PATH || "./movies";
 const port = env.PORT || 3000;
 const app = express();
 
-app.use(express.static(folder), serveIndex(folder, { icons: true }));
+app.use(`/${folder}`, express.static(folder));
+app.get("/", function (req, res) {
+  files = [];
+  scanDirectory(folder);
+  let links = "";
+  files.map((f) => (links = links + `<a href="${f}">${f}</a> <br/>`));
+  res.send(links);
+});
 app.listen(port);
 
 console.log(`Running on Port http://${ip.address()}:${port}/`);
